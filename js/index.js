e2a2b2ac1e9721968613cfc9de5257715b6328bb
iamchiragc <chiragsinhchavda01@gmail.com>
2022-10-19 13:12:57 +0530

    api setting...

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ console.log('iamindex...')
 const url = 'https://newsapi.org/v2/top-headlines?' +
 	'country=in&' +
 	'apiKey=991a0d09788e411a9715bb6cc4397a55';
-
-fetch(url)
+let req = new Request(url);
+fetch(req)
 	.then(function (response) {
 		let data = response.json();
 		data.then(
@@ -11,10 +11,9 @@ fetch(url)
 				let articles = getData.articles;
 				let blogs = document.getElementById('blog');
 				articles.forEach(element => {
-					console.log(element.url)
 					blogs.innerHTML += `<div class="blogs">
 											<div class="img">
-												<img src=${element.urlToImage} alt="blog" srcset="">
+												<img src=${element.urlToImage} alt="Image Not Available.." srcset="">
 											</div>
 											<h3 id="title" class="titles"><a target="_blank" href=${element.url}>${element.title}</a></h3>
 											<p id="discription" class="discs">${element.description}</p>
